51287f4fe78d2a2ca80d68d23e4d4f4cdd560e9d
ShivashankarL <ss.lakshminarayan@myeduate.com>
2025-11-13 13:34:18 +0530

    Changes

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shivashankar-dev-dot/gin-backend/models"
+)
+
+var todos = []models.Todo{
+	{
+		ID:        1,
+		Title:     "Computers",
+		Completed: false,
+	},
+
+	{
+		ID:        2,
+		Title:     "Social Science",
+		Completed: false,
+	},
+}
+
+func getTodos(c *gin.Context) {
+
+	fmt.Println(c.Request.Method)
+	c.JSON(200, gin.H{
+		"todos": todos,
+	})
+}
+
+func createTodo(c *gin.Context) {
+	var newTodo models.Todo
+	if err := c.ShouldBindJSON(&newTodo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	newTodo.ID = uint(len(todos) + 1)
+
+	todos := append(todos, newTodo)
+
+	fmt.Printf("%v", todos)
+	c.JSON(http.StatusCreated, gin.H{
+		"todos": todos,
+	})
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/todos", getTodos)
+	r.POST("/todos", createTodo)
+
+	r.Run(":8000")
+}
